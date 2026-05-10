9f99b46684647f504dd2ad05f96c3e596733eb87
NeenNin <neens2592549@gmail.com>
2026-05-10 17:47:38 +0700

    เขียนgemmanagement

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,43 @@
+#include "Board.h"
 //board+gem
+Board::Board(){
+    board_gem.White = 7;
+    board_gem.Blue = 7;
+    board_gem.Green = 7;
+    board_gem.Red = 7;
+    board_gem.Black = 7;
+    board_gem.Gold = 5;
+}
+
+bool Board::gemmanagement(std::string Action, Gem g){
+    if(Action == "take"){
+        if(board_gem.White < g.White){} return false;
+        if(board_gem.Blue < g.Blue){} return false;
+        if(board_gem.Green < g.Green){} return false;
+        if(board_gem.Red < g.Red){} return false;
+        if(board_gem.Black < g.Black){} return false;
+        if(board_gem.Gold < g.Gold){} return false;
+
+        board_gem.White -= g.White;
+        board_gem.Blue -= g.Blue;
+        board_gem.Green -= g.Green;
+        board_gem.Red -= g.Red;
+        board_gem.Black -= g.Black;
+        board_gem.Gold -= g.Gold;
+        return true;
+    }
+
+    if(Action == "return"){
+        board_gem.White += g.White;
+        board_gem.Blue += g.Blue;
+        board_gem.Green += g.Green;
+        board_gem.Red += g.Red;
+        board_gem.Black += g.Black;
+        board_gem.Gold += g.Gold;
+        return true;
+    }
+}
+
+void Board::replacecard(){
+
+}
--- Board.h
+++ Board.h
@@ -1,3 +1,16 @@
 #pragma once //do not delete this line
-
+#include "Card.h"
+#include <vector>
+#include <string>
 //board ไม่ต้องเขียนgemแล้ว คีย์เขียนละ
+class Board{
+    private:
+        Gem board_gem;
+        std::vector<NobilityCard> board_noblility;
+        std::vector<DevelopmentCard> board_development;
+        //std::vector<Deck> board_deck;
+
+    public:
+        bool gemmanagement(std::string Action, Gem g);
+        void replacecard();
+};
